685182bc4f85d7b0d7902938991f3b83891a6cd4
sevellavikranthreddy <sevellavikranthreddy@gmail.com>
2026-04-20 11:20:48 +0530

    first commit

---
+++ fact.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n = 5,b=n,fact=1;
+  while(n>0)
+  {
+    fact*=n
+    n--;
+
+  }
+cout<<"factorial of "<<b<<" is "<<fact;
+return 0;
+}
